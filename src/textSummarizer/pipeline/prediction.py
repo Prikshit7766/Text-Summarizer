a4ce11b49b15e9a3fb235fd0359c18b6689d6009
Prikshit7766 <prikshitsharma8024@gmail.com>
2024-05-05 02:27:00 +0530

    updated prediction.py logic

--- src/textSummarizer/pipeline/prediction.py
+++ src/textSummarizer/pipeline/prediction.py
@@ -2,23 +2,26 @@ from ..config.configuration import ConfigurationManager
 from transformers import AutoTokenizer
 from transformers import pipeline
 
-
 class PredictionPipeline:
     def __init__(self):
         self.config = ConfigurationManager().get_model_evaluation_config()
+        self.tokenizer = None
+        self.pipe = None
 
+    def lazy_load(self):
+        if self.tokenizer is None:
+            self.tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
+        if self.pipe is None:
+            gen_kwargs = {"length_penalty": 0.8, "num_beams": 8, "max_length": 128}
+            self.pipe = pipeline("summarization", model=self.config.model_path, tokenizer=self.tokenizer)
 
-    
-    def predict(self,text):
-        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
-        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
-
-        pipe = pipeline("summarization", model=self.config.model_path,tokenizer=tokenizer)
+    def predict(self, text):
+        self.lazy_load()
 
         print("Dialogue:")
         print(text)
 
-        output = pipe(text, **gen_kwargs)[0]["summary_text"]
+        output = self.pipe(text)[0]["summary_text"]
         print("\nModel Summary:")
         print(output)
 
